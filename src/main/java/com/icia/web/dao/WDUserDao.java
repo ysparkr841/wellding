ee0297dac3d6e2915cb725152d4ec3aee530ef06
parkeuisoo <ysparkr841@gmail.com>
2021-12-31 10:06:24 +0900

    [update] wduserDao

--- src/main/java/com/icia/web/dao/WDUserDao.java
+++ src/main/java/com/icia/web/dao/WDUserDao.java
@@ -6,15 +6,17 @@ import org.springframework.stereotype.Repository;
 import com.icia.web.model.WDUser;
 
 
-@Repository("WDUserDao")
 public interface WDUserDao
 {
+
 	public int userInsert(WDUser wduser);
 	
-	
+
 	public int userUpdate(WDUser wduser);
 	
-	
+
+
+
 	public WDUser userSelect(String userId);
 	
 	public int wdUserIdCount(String userId);
