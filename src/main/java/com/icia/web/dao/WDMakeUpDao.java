ceb582bf12cbfe0c9d0e2dd75634ab1b821a46c3
parkeuisoo <ysparkr841@gmail.com>
2022-01-01 14:49:14 +0900

    [add][fix] WDMakeUP 관련 .java .dao .xml 작성, WDStudioDao.xml의 오타 수정 PES

---
+++ src/main/java/com/icia/web/dao/WDMakeUpDao.java
@@ -0,0 +1,12 @@
+package com.icia.web.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.icia.web.model.WDMakeUp;
+
+@Repository("WDMakeUpDao")
+public interface WDMakeUpDao 
+{
+	//메이크업 업체 총 수 시작
+	public long makeUpListCount(WDMakeUp wdMakeUp);
+}
